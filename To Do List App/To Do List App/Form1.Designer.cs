4d95369e6abc39b06193f1c56c8865a802d7f6f7
Oluwaniyi Emmanuel <Oluwaniyiemmanuel22@gmail.com>
2026-06-08 09:33:29 +0100

    Add files via upload

---
+++ To Do List App/To Do List App/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace To_Do_List_App
+{
+    partial class ToDoList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.TitletextBox = new System.Windows.Forms.TextBox();
+            this.DescriptiontextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Newbutton = new System.Windows.Forms.Button();
+            this.Editbutton = new System.Windows.Forms.Button();
+            this.Savebutton = new System.Windows.Forms.Button();
+            this.Deletebutton = new System.Windows.Forms.Button();
+            this.ToDoListview = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.ToDoListview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Vineta BT", 28.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1113, 72);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "To Do List";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // TitletextBox
+            // 
+            this.TitletextBox.Location = new System.Drawing.Point(4, 110);
+            this.TitletextBox.Name = "TitletextBox";
+            this.TitletextBox.Size = new System.Drawing.Size(1121, 22);
+            this.TitletextBox.TabIndex = 1;
+            // 
+            // DescriptiontextBox
+            // 
+            this.DescriptiontextBox.Location = new System.Drawing.Point(4, 167);
+            this.DescriptiontextBox.Name = "DescriptiontextBox";
+            this.DescriptiontextBox.Size = new System.Drawing.Size(1121, 22);
+            this.DescriptiontextBox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(9, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(1116, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Title:";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(9, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(1116, 29);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Description:";
+            // 
+            // Newbutton
+            // 
+            this.Newbutton.Location = new System.Drawing.Point(4, 195);
+            this.Newbutton.Name = "Newbutton";
+            this.Newbutton.Size = new System.Drawing.Size(277, 38);
+            this.Newbutton.TabIndex = 5;
+            this.Newbutton.Text = "New";
+            this.Newbutton.UseVisualStyleBackColor = true;
+            this.Newbutton.Click += new System.EventHandler(this.Newbutton_Click);
+            // 
+            // Editbutton
+            // 
+            this.Editbutton.Location = new System.Drawing.Point(287, 195);
+            this.Editbutton.Name = "Editbutton";
+            this.Editbutton.Size = new System.Drawing.Size(277, 38);
+            this.Editbutton.TabIndex = 6;
+            this.Editbutton.Text = "Edit";
+            this.Editbutton.UseVisualStyleBackColor = true;
+            this.Editbutton.Click += new System.EventHandler(this.Editbutton_Click);
+            // 
+            // Savebutton
+            // 
+            this.Savebutton.Location = new System.Drawing.Point(570, 195);
+            this.Savebutton.Name = "Savebutton";
+            this.Savebutton.Size = new System.Drawing.Size(277, 38);
+            this.Savebutton.TabIndex = 7;
+            this.Savebutton.Text = "Save";
+            this.Savebutton.UseVisualStyleBackColor = true;
+            this.Savebutton.Click += new System.EventHandler(this.Savebutton_Click);
+            // 
+            // Deletebutton
+            // 
+            this.Deletebutton.Location = new System.Drawing.Point(853, 195);
+            this.Deletebutton.Name = "Deletebutton";
+            this.Deletebutton.Size = new System.Drawing.Size(277, 38);
+            this.Deletebutton.TabIndex = 8;
+            this.Deletebutton.Text = "Delete";
+            this.Deletebutton.UseVisualStyleBackColor = true;
+            this.Deletebutton.Click += new System.EventHandler(this.Deletebutton_Click);
+            // 
+            // ToDoListview
+            // 
+            this.ToDoListview.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.ToDoListview.BackgroundColor = System.Drawing.Color.White;
+            this.ToDoListview.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ToDoListview.Location = new System.Drawing.Point(4, 239);
+            this.ToDoListview.Name = "ToDoListview";
+            this.ToDoListview.RowHeadersWidth = 51;
+            this.ToDoListview.RowTemplate.Height = 24;
+            this.ToDoListview.Size = new System.Drawing.Size(1126, 320);
+            this.ToDoListview.TabIndex = 9;
+            // 
+            // ToDoList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(1139, 571);
+            this.Controls.Add(this.ToDoListview);
+            this.Controls.Add(this.Deletebutton);
+            this.Controls.Add(this.Savebutton);
+            this.Controls.Add(this.Editbutton);
+            this.Controls.Add(this.Newbutton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.DescriptiontextBox);
+            this.Controls.Add(this.TitletextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "ToDoList";
+            this.Text = "To Do List";
+            this.Load += new System.EventHandler(this.ToDoList_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ToDoListview)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TitletextBox;
+        private System.Windows.Forms.TextBox DescriptiontextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button Newbutton;
+        private System.Windows.Forms.Button Editbutton;
+        private System.Windows.Forms.Button Savebutton;
+        private System.Windows.Forms.Button Deletebutton;
+        private System.Windows.Forms.DataGridView ToDoListview;
+    }
+}
+
---
+++ To Do List App/To Do List App/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace To_Do_List_App
+{
+    public partial class ToDoList : Form
+    {
+        public ToDoList()
+        {
+            InitializeComponent();
+        }
+
+        DataTable todoList = new DataTable();
+        bool isEditing = false;
+        private void ToDoList_Load(object sender, EventArgs e)
+        { 
+
+            todoList.Columns.Add("title");
+            todoList.Columns.Add("Description");
+
+            ToDoListview.DataSource = todoList;
+
+        }
+
+        private void Newbutton_Click(object sender, EventArgs e)
+        {
+            TitletextBox.Text = "";
+            DescriptiontextBox.Text = "";
+
+        }
+
+        private void Editbutton_Click(object sender, EventArgs e)
+        {
+            isEditing = true;
+            TitletextBox.Text = todoList.Rows[ToDoListview.CurrentCell.RowIndex].ItemArray[0].ToString();
+            DescriptiontextBox.Text = todoList.Rows[ToDoListview.CurrentCell.RowIndex].ItemArray[1].ToString();
+        }
+
+        private void Deletebutton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                todoList.Rows[ToDoListview.CurrentCell.RowIndex].Delete();
+            }
+            catch(Exception ex) 
+            {
+                Console.WriteLine("Error: " + ex);
+            }
+        }
+
+        private void Savebutton_Click(object sender, EventArgs e)
+        {
+            if(isEditing)
+            {
+                todoList.Rows[ToDoListview.CurrentCell.RowIndex]["Title"] = TitletextBox.Text;
+                todoList.Rows[ToDoListview.CurrentCell.RowIndex]["Description"] = DescriptiontextBox.Text;
+            }
+            else
+            {
+                todoList.Rows.Add(TitletextBox.Text, DescriptiontextBox.Text);
+            }
+            TitletextBox.Text = "";
+            DescriptiontextBox.Text = "";
+            isEditing = false;
+    }
+}
+}
